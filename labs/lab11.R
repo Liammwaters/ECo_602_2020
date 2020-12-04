0e4d49ec19415b936e9ef673869da4d602b614c4
Liammwaters <70780362+Liammwaters@users.noreply.github.com>
2020-12-04 13:16:58 -0500

    latest updata

--- labs/lab11.R
+++ labs/lab11.R
@@ -3,22 +3,25 @@ require(here)
 birds = read.csv(here("data", "bird.sub.csv"))
 hab = read.csv(here("data", "hab.sub.csv"))
 birdhab = merge(birds, hab)
-dim(birdhab)
-summary(birdhab)
-head(birdhab)
+
+
 
 
 plot(BRCR ~ ls, data = birdhab)
 fit_1 = lm(BRCR ~ ls, data = birdhab)
 abline(fit_1)
 
-coef(fit_1)
+#assign the summary table to an object
+sum_fit_1 = summary(fit_1)
+
+#this allows you to isolate the parts of the summary table such as:
+sum_fit_1$coefficients
 
 
 
 linear = function(x, y_int, slope)
 {
-  return(numeric())
+  return(y = y_int + slope * x)
 }
 
 linear(x = 1, y_int = 1, slope = 1)
